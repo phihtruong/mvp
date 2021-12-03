30923e01457d158feb63544e0f11daa4a12c2924
Phi Truong <phihtruong00@gmail.com>
2021-12-03 11:44:12 -0800

    Project initialization

---
+++ client/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DailyView from './DailyView.jsx';
+
+const App = () => {
+  return (
+    <div>
+      <DailyView />
+    </div>
+  )
+}
+
+export default App;
---
+++ client/src/components/DailyView.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DailyView = () => {
+  return (
+    <span> Get your sh*t together! </span>
+  )
+}
+
+export default DailyView;
